8e2206bdb24a3048063c5bec9c69f63aaa5ca6e9
Dariga <beysenbaydariga@gmail.com>
2026-03-02 16:37:24 +0500

    files were added

---
+++ online-store/src/app/components/product-list/product-list.component.ts
@@ -0,0 +1,18 @@
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Product } from '../../models/product.model';
+import { ProductItemComponent } from '../product-item/product-item.component';
+
+@Component({
+  selector: 'app-product-list',
+  standalone: true,
+  imports: [CommonModule, ProductItemComponent],
+  templateUrl: './product-list.component.html',
+  styleUrl: './product-list.component.css'
+})
+export class ProductListComponent {
+  @Input() products: Product[] = [];
+  handleRemove(productId: number) {
+    this.products = this.products.filter(p => p.id !== productId);
+  }
+}
